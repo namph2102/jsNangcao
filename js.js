2255ae63cde55c03ff88a80c1bcdbc558f73e8d2
Nam <namph2102@gmail.com>
2023-01-05 23:34:07 +0700

    upload lab2

--- js.js
+++ js.js
@@ -1,24 +1,28 @@
 const gameEvents = new Map([
-    [17, 'GOAL'],
+    [17, '⚽ GOAL'],
     [36, 'SUBTITUTION'],
-    [47, 'GOAL'],
-    [61, 'YELLOW CARD'],
-    [64, 'RED CARD'],
-    [69, 'SUBTITUTION'],
-    [70, 'GOAL'],
-    [80, 'GOAL'],
-    [91, 'GOAL'],
+    [47, '⚽ GOAL'],
+    [61, 'SUBTITUTION'],
+    [64, 'YELLOW CARD'],
+    [69, 'RED CARD'],
+    [70, 'SUBTITUTION'],
+    [72, '⚽ GOAL'],
+    [76, 'SUBTITUTION'],
+    [80, '⚽ GOAL'],
+    [90, 'YELLOW CARD'],   
 ]
 );
-
-// 1. Events 
-const events=new Set();
-// console.log(gameEvents);
-const newArray=[...new Set(gameEvents)];
-for(const [key,value] of gameEvents){
-    events.add(value);
-}
-console.log(`Check from minutes 64 :${ gameEvents.has(64)}`);
+// 1. tạo mảng
+const newArray=[...new Set(gameEvents.values())];
+console.log(newArray);
+//2. 
 gameEvents.delete(64);
-// console.log(gameEvents);
-console.log('An event ')
+//3.
+const time=[...new Set(gameEvents.keys())][gameEvents.size-1]
+console.log(`"1 sự kiện xảy ra, trung bình mỗi ${(time/gameEvents.size).toFixed(2)} phút`);
+
+//4. 
+
+for(const [key,value] of gameEvents){
+    console.log(`[ ${ key <= 45 && 'FIRST' || 'SECOND'} HALF] ${key}: ${value}]`)
+}
