6e9ecb038784d0fb6b4684c10e1e42f294cc440b
Nam <namph2102@gmail.com>
2023-01-09 20:48:50 +0700

    uploadlab2

--- js.js
+++ js.js
@@ -1,3 +1,13 @@
-const vnEline={
-  name:"Việt Nam Eline",
+const vnEline = {
+  name: "Việt Nam Eline",
+  iataCode: "VN",
+  bookings(charCode, fullName) {
+    return `I will booking ${this.name} with code ${this.iataCode + charCode} by ${fullName}`;
+  }
 }
+const camEline={
+  name: "Capuchia Eline",
+  iataCode: "CP",
+}
+const book=vnEline.bookings;
+console.log(book.apply(camEline,[125,"Phạm hoài Nam"]));
