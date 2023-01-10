6552d3b695e41e3c5c317875730aad57d440385d
Nam <namph2102@gmail.com>
2023-01-10 23:14:54 +0700

    upload

--- js.js
+++ js.js
@@ -1,13 +1,31 @@
-const vnEline = {
-  name: "Việt Nam Eline",
-  iataCode: "VN",
-  bookings(charCode, fullName) {
-    return `I will booking ${this.name} with code ${this.iataCode + charCode} by ${fullName}`;
-  }
+const filght={
+    name:"VietName Airline",
+    itataCode:"VN",
+    books(charCode,fullname){
+        console.log(`${this.name} Xin chào quý khách ${fullname} có mã số ghế ${this.itataCode+charCode}`);
+    }
 }
-const camEline={
-  name: "Capuchia Eline",
-  iataCode: "CP",
+const newFilght={
+    name:"Malaysia AirLine",
+    itataCode:"ML"
 }
-const book=vnEline.bookings;
-console.log(book.apply(camEline,[125,"Phạm hoài Nam"]));
+const bookings=filght.books;
+
+bookings.apply(newFilght,[12542,"Phạm Hoài Nam"]);;
+newFilght.filght=300;
+newFilght.planes=function(){
+    this.filght++;
+    console.log(this);
+}
+document.querySelector('.plan').addEventListener('click',newFilght.planes.bind(newFilght))
+const bookVN23=bookings.bind(filght,23);
+bookVN23('catuxi'),
+bookVN23('James Makeyta')
+const addTax=(rate,value)=>rate+value*rate;
+const TaxVN=addTax.bind(null,100)
+const addTaxtRare=(rate)=>(value)=>rate+rate*value;
+const vnTaxt=addTaxtRare(100)
+console.log(vnTaxt(0.23));
+console.log(vnTaxt(0.5));
+
+
