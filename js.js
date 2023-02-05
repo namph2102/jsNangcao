90fd43f3cae00d1cccc34da5e527d28ba0014125
Nam <namph2102@gmail.com>
2023-02-05 13:00:58 +0700

    'upload'

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
 const movements=[200,450,-400,3000,-650,-130,70,1300];
+const $=document.querySelector.bind(document);
+
+$('.balance__box').innerHTML=movements.map((mov,index)=>{
+    return `<div class="balance__box--item">
+    <div class="balance__box--item__type ${mov>0?"desposit":"drawal"}">
+        ${index+1} ${mov>0?"DESPOSIT":"WITHDRAWAL"}
+    </div>
+    <div class="balance__box--item__cost">
+        ${Math.abs(mov)} đ
+    </div>
+</div>`
+})
+const arrr=[[1,2,3],4,5,[6,3,7,8],8];
+console.log(arrr.flat());
+console.log(arrr.flat().sort((a,b)=>a-b));
+console.log(arrr.flat().sort((a,b)=>b-a));
